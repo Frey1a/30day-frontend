17889c6b6a912ef4acfcff785b2b1476833d2070
Frey1a <78993276+Frey1a@users.noreply.github.com>
2022-04-17 21:00:01 +0700

    Done Day12!

--- day/day12/js/app.js
+++ day/day12/js/app.js
@@ -1,1 +1,45 @@
+const rangeBar = document.querySelector(".ranger-bar")
+const range = document.querySelector(".ranger")
+
+const body = document.querySelector("body")
+
+function updateWidth(percent) {
+	rangeBar.style.width = `${percent}%`
+}
+function updateBackground(percent) {
+	alphaColor = percent / 100
+	body.style.background = `rgba(0,0,0,${alphaColor})`
+}
+
+range.addEventListener("mousemove", function (event) {
+	var processWidth = event.pageX - this.offsetLeft
+	var percent = processWidth / this.offsetWidth * 100
+	percent = Math.round(percent)
+
+	updateWidth(percent)
+	updateBackground(percent)
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
