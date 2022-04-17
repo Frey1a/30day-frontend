ca5c08bb1890fd4d95298a9391dcce0d6ca89e12
Frey1a <78993276+Frey1a@users.noreply.github.com>
2022-04-17 22:12:14 +0700

    Done day13!

--- day/day13/js/app.js
+++ day/day13/js/app.js
@@ -1,4 +1,24 @@
-const imgInput = document.querySelector('.preview img')
+const inputImg = document.querySelector("#input-img")
 const preview = document.querySelector(".preview")
+
+inputImg.addEventListener("change", function (e) {
+	var fileImg = inputImg.files[0]
+
+	if (!fileImg) {
+		return
+	}
+	if (!(/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i).test(fileImg.name)) {
+		return
+	}
+
+	var img = document.createElement("img")
+	img.src = URL.createObjectURL(inputImg.files[0])
+	preview.appendChild(img)
+
+})
+
+
+
+
 
 
