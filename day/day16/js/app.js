5d0644adf282166385a43a415eb31030c82c2c39
Frey1a <78993276+Frey1a@users.noreply.github.com>
2022-04-19 23:32:23 +0700

    Done day16!

--- day/day16/js/app.js
+++ day/day16/js/app.js
@@ -1,1 +1,32 @@
+const listScroll = document.querySelectorAll(".show-scroll")
+const divHome = document.querySelector("#home div")
+setTimeout(function () {
+	divHome.classList.add("start")
+}, 500)
+
+window.onscroll = runScrool
+
+function showElementScroll(element) {
+	let rect = element.getClientRects()[0]
+	let windowHeight = window.innerHeight
+	if (!(rect.bottom < 0 || rect.top > windowHeight)) {
+		element.classList.add("start")
+
+	} else {
+		element.classList.remove("start")
+	}
+
+
+}
+function runScrool() {
+	listScroll.forEach(input => {
+		showElementScroll(input)
+	})
+
+}
+
+
+
+
+
 
