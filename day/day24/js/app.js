8742dd660039dd0b6a82c2bb8abf5d8bd7c9a304
Frey1a <78993276+Frey1a@users.noreply.github.com>
2022-04-24 08:53:23 +0700

    Done day 24!

--- day/day24/js/app.js
+++ day/day24/js/app.js
@@ -1,33 +1,40 @@
-const container = document.querySelector('.container')
-const SQUARES = 200
+let container = document.querySelector(".container")
+let squares = 200
 
-for (let i = 0; i < SQUARES; i++) {
-	const square = document.createElement('div')
-	square.classList.add('square')
+for (let i = 0; i < squares; i++) {
+	let square = document.createElement("div")
+	square.classList.add("square")
 
-	square.addEventListener('mouseover', () => setColor(square))
-
-	square.addEventListener('mouseout', () => removeColor(square))
+	//animation
+	square.addEventListener("mouseover", () => setColor(square))
+	square.addEventListener("mouseout", () => removeColor(square))
 
 	container.appendChild(square)
 }
 
 function setColor(element) {
-	const color = generateRandomColor()
-	element.style.background = color
+	const color = getColorRandom()
+	element.style.background = `${color}`
 	element.style.boxShadow = `0 0 10px ${color}, 0 0 100px ${color}`
+
 }
 
 function removeColor(element) {
-	element.style.background = '#1d1d1d'
-	element.style.boxShadow = '0 0 2px #000'
+	element.style.background = "#1c1a1a"
+	element.style.boxShadow = `0 0 2px #000000`
+
 }
 
-function generateRandomColor() {
-	let letters = '0123456789ABCDEF'
-	let color = '#'
+
+function getColorRandom() {
+	let letters = "0123456789ABCDEF"
+	let color = "#"
 	for (let i = 0; i < 6; i++) {
 		color += letters[Math.floor(Math.random() * 16)]
 	}
+
 	return color
 }
+
+
+
