04347d384c4676553241595130d9cfc3d2c6b85b
Freyja <78993276+Frey1a@users.noreply.github.com>
2022-04-26 23:53:07 +0700

    Done day25!

--- day/day25/js/app.js
+++ day/day25/js/app.js
@@ -1,90 +1,150 @@
 const player = document.querySelector(".player")
+const previewVideo = player.querySelector(".player__preview")
 const playerControls = player.querySelector(".player__controls")
 const video = player.querySelector(".player__video")
-const progess = player.querySelector(".player__progess")
-const progessBar = player.querySelector(".progess__bar")
-
+const progress = player.querySelector(".player__progress")
+const progressBar = player.querySelector(".progress__bar")
+const time = player.querySelector(".player__time")
 const toggle = player.querySelector(".toggle")
 const skipData = player.querySelectorAll("[data-skip]")
-const volumeButton = player.querySelector("player__volume i")
+const volumeButton = player.querySelector(".volume__button")
 const volumeBar = player.querySelector(".player__volume input")
 
+//set default volume 
+video.volume = volumeBar.defaultValue
 
+setInterval(() => video.volume = volumeBar.value, 1)
+
+//toggle play
 function togglePlay() {
-	if (video.paused) {
-		video.play()
-		toggle.innerHTML = "<i class='bx bx-pause' ></i>"
-	} else {
-		video.pause()
-		toggle.innerHTML = "<i class='bx bx-play'></i>"
-	}
+    if (video.paused) {
+        video.play()
+        toggle.innerHTML = "<i class='bx bx-pause' ></i>"
+    } else {
+        video.pause()
+        toggle.innerHTML = "<i class='bx bx-play'></i>"
+    }
 }
-console.log(skipData)
-console.dir(video)
-console.dir(volumeBar)
 
+//toggle volume
+function toggleVolume() {
+    if (volumeButton.id == "v_on") {
+        var currentVolume = video.volume
+        volumeButton.removeAttribute("id")
+        volumeButton.setAttribute("id", "v_off")
+        volumeButton.innerHTML = `<i class='bx bxs-volume-mute'></i>`
+        volumeBar.value = 0
+        video.volume = volumeBar.value
+    } else if (volumeButton.id == "v_off") {
+        volumeButton.removeAttribute("id")
+        volumeButton.setAttribute("id", "v_on")
+        volumeButton.innerHTML = `<i class='bx bxs-volume-full' ></i>`
+        volumeBar.value = currentVolume
+        video.volume = volumeBar.value
+    }
+}
+
+//show control ì hover or event
 function showControls() {
-	playerControls.style.transform = "translateY(-100%)"
+    playerControls.style.transform = "translateY(-100%)"
 
-	let timeOutControls = setTimeout(function () {
-		playerControls.style.transform = ""
-	}, 3000)
+    let timeOutControls = setTimeout(function() {
+        playerControls.style.transform = ""
+    }, 3000)
 
-	document.addEventListener("keydown", function () {
-		clearTimeout(timeOutControls)
-	})
+    document.addEventListener("keydown", function() {
+        clearTimeout(timeOutControls)
+    })
 
 }
 
+
+//format show time
 function formatTime(time) {
-	let second = Math.floor(time / 60)
-	let miniute = Math.floor(time - second * 60)
-	let miniuteValue, secondValue
-	miniuteValue = miniute < 10 ? "0" + miniute : miniute
-	secondValue = secondValue < 10 ? "0" + secondValue : miniute
-	let mediaTime = miniuteValue + ":" + secondValue
-	return mediaTime
+    let minutes = Math.floor(time / 60)
+    let seconds = Math.floor(time - minutes * 60)
+    let minuteValue, secondValue
 
+    minuteValue = minutes < 10 ? '0' + minutes : minutes
+    secondValue = seconds < 10 ? '0' + seconds : seconds
+
+    let mediaTime = minuteValue + ':' + secondValue
+    return mediaTime
 }
 
-function changeVolume(key) {
-	let valueVolume = volumeBar.value
-	let currentValue = 0.1
-	switch (key) {
-		case key == 38:
-			valueVolume += currentValue
-			break;
-		case key == 40:
-			valueVolume -= currentValue
-			break
-	}
-	video.volume = valueVolume
 
 
+// show time
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100
+    progressBar.style.width = `${percent}%`
+
+    time.innerHTML = `${formatTime(video.currentTime)} / ${formatTime(
+		video.duration
+	)}`
 }
+
+// function for click progress video
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+    video.currentTime = scrubTime
+}
+
+
+
+
+
 
 // Evenlistener
 // skipData.addEventListener
+
+video.addEventListener('timeupdate', handleProgress)
 video.addEventListener("click", togglePlay)
-document.addEventListener("keydown", function (e) {
-	if (e.which == 32) {
-		player.onmouseover
-		togglePlay()
-		showControls()
-	} else if (e.which == 75) {
-		togglePlay()
-		showControls()
-	} else if (e.which == 40) {
-		changeVolume(e.which)
-	} else if (e.which == 38) {
-		changeVolume(e.which)
-	}
+
+
+progress.addEventListener("click", scrub)
+
+
+skipData.forEach((button) =>
+    button.addEventListener('click', function() {
+        video.currentTime += +this.dataset.skip
+    })
+)
+
+
+document.addEventListener("keydown", function(e) {
+
+    switch (e.which) {
+        case 32:
+            togglePlay()
+            showControls()
+            break;
+        case 75:
+            togglePlay()
+            showControls()
+            break;
+        case 77:
+            toggleVolume()
+            showControls()
+
+    }
 })
+
 toggle.addEventListener("click", togglePlay)
-volumeBar.addEventListener("change", () => {
-	video.volume = volumeBar.value
+
+volumeBar.addEventListener("click", () => {
+    video.volume = volumeBar.value
+    volumeBar.defaultValue = video.volume
 })
 
 
+// volume
+volumeButton.addEventListener("click", toggleVolume)
 
-
+//hiden preVideo
+previewVideo.addEventListener("click", () => {
+    previewVideo.style.display = "none"
+    video.classList.remove("hiden")
+    playerControls.classList.remove("hiden")
+    video.click()
+})
