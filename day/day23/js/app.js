f228e31a5e1f1707a1c8420903f7eff94a253ee1
Freyja <78993276+Frey1a@users.noreply.github.com>
2022-04-28 18:47:47 +0700

    Done day23!

--- day/day23/js/app.js
+++ day/day23/js/app.js
@@ -5,29 +5,29 @@ const result = document.querySelector('.zoomer .result')
 const size = 4
 
 wrapImg.forEach((item) => {
-	item.addEventListener('mousemove', function (e) {
-		result.classList.remove('hide')
+    item.addEventListener('mousemove', function(e) {
+        result.classList.remove('hide')
 
-		const img = item.querySelector('img')
+        const img = item.querySelector('img')
 
-		let x = (e.offsetX / this.offsetWidth) * 100
-		let y = (e.offsetY / this.offsetHeight) * 100
+        let x = (e.offsetX / this.offsetWidth) * 100
+        let y = (e.offsetY / this.offsetHeight) * 100
 
-		// move result
-		let posX = e.pageX - zoomer.offsetLeft
-		let posY = e.pageY - zoomer.offsetTop
+        // move result
+        let posX = e.pageX - zoomer.offsetLeft
+        let posY = e.pageY - zoomer.offsetTop
 
-		result.style.cssText = `
-			background-image: url(${img.src});
-			background-size: ${img.width * size}px ${img.height * size}px;
-			background-position : ${x}% ${y}%;
-			left: ${posX}px;
-			top: ${posY}px;
-		`
-	})
+        result.style.cssText = `
+        	background-image: url(${img.src});
+        	background-size: ${img.width * size}px ${img.height * size}px;
+        	background-position : ${x}% ${y}%;
+        	left: ${posX}px;
+        	top: ${posY}px;
+        `
+    })
 
-	item.addEventListener('mouseleave', function (e) {
-		result.style = ``
-		result.classList.add('hide')
-	})
+    item.addEventListener('mouseleave', function(e) {
+        result.style = ``
+        result.classList.add('hide')
+    })
 })
