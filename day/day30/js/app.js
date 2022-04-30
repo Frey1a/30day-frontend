a82d4f8a28fc0ab9671f02f754fc96df0288a536
Frey1a <78993276+Frey1a@users.noreply.github.com>
2022-04-30 16:45:39 +0700

    Done day30!

--- day/day30/js/app.js
+++ day/day30/js/app.js
@@ -1,8 +1,11 @@
-const wrapper = document.querySelector('.wrapper')
+const wrapper = document.querySelector(".wrapper")
 let isMouseDown = false
 let startX, scrollLeft
+let speedScroll = 3
 
-wrapper.addEventListener('mousedown', (e) => {
+
+
+wrapper.addEventListener("mousedown", (e) => {
 	isMouseDown = true
 	startX = e.pageX - wrapper.offsetLeft
 	scrollLeft = wrapper.scrollLeft
@@ -16,11 +19,18 @@ wrapper.addEventListener('mouseup', () => {
 	isMouseDown = false
 })
 
-wrapper.addEventListener('mousemove', (e) => {
+wrapper.addEventListener("mousemove", (e) => {
 	if (!isMouseDown) return
 
 	const x = e.pageX - wrapper.offsetLeft
-	// 3 là tốc độ scroll
-	const walk = (x - startX) * 3
+
+
+	const walk = (x - startX) * speedScroll
 	wrapper.scrollLeft = scrollLeft - walk
+
 })
+
+
+
+
+
